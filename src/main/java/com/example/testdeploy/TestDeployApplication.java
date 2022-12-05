1dec9bb66e9cc90f7d81f62fc9597a50d6bffd94
Sheena Abraham <SheenaPAbraham@gmail.com>
2022-12-05 11:18:28 -0500

    updated to jdk 19

--- src/main/java/com/example/testdeploy/TestDeployApplication.java
+++ src/main/java/com/example/testdeploy/TestDeployApplication.java
@@ -1,5 +1,6 @@
 package com.example.testdeploy;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
--- src/test/java/com/example/testdeploy/TestDeployApplicationTests.java
+++ src/test/java/com/example/testdeploy/TestDeployApplicationTests.java
@@ -1,13 +1,14 @@
-package com.example.testdeploy;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class TestDeployApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package com.example.testdeploy;
+//
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class TestDeployApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
